efb8ee9a64e3691d16dcc40ff0fea5a590316c73
YUSRA SHAHID <99241818+yusrashahid205@users.noreply.github.com>
2022-02-10 14:59:39 +0500

    Add files via upload

---
+++ input and output.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;   // Taking input
+    cout << "The number is: " << num;
+    return 0;
