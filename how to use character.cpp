69d990d48fcb5435173300d6939fbf26581c7f71
YUSRA SHAHID <99241818+yusrashahid205@users.noreply.github.com>
2022-02-10 15:06:42 +0500

    Add files via upload

---
+++ how to use character.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // initializing a variable
+    char ch = 'h';
+
+    // printing the  variable
+    cout << "Character = " << ch << endl;
+
+    return 0;
